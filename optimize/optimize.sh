7da52c26fc06eabfc71c50fb2fa1d39f11745673
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-05-18 14:27:39 +0300

    Fix

--- optimize/optimize.sh
+++ optimize/optimize.sh
@@ -6,4 +6,4 @@ echo "Optimizing ${COMPONENT_NAME}:${JAR_PATH} on java version ${JAVA_VERSION}";
 dnf install -y "java-${JAVA_VERSION}-openjdk-devel" "java-${JAVA_VERSION}-openjdk-jmods" binutils;
 "/usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/jdeps" "--multi-release=${JAVA_VERSION}" -quiet --print-module-deps --ignore-missing-deps --recursive "${JAR_PATH}" > jre_modules.out;
 /usr/bin/xargs -a jre_modules.out "/usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/jlink" --compress=zip-9 --no-header-files --no-man-pages --strip-debug "--output=/tmp/${COMPONENT_NAME}/" --add-modules;
-cp -arv "/tmp/${COMPONENT_NAME}/" "/opt/teragrep/${COMPONENT_NAME}/";
+cp -arv "/tmp/${COMPONENT_NAME}/*" "/opt/teragrep/${COMPONENT_NAME}/";
