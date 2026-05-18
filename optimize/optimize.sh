7633c7f1323b7403727170c548169c98d8cd4d0a
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-05-18 15:05:30 +0300

    Fix

--- optimize/optimize.sh
+++ optimize/optimize.sh
@@ -5,5 +5,5 @@ COMPONENT_PATH="${3}";
 echo "Optimizing ${JAR_PATH} on java version ${JAVA_VERSION} to ${COMPONENT_PATH}";
 dnf install -y "java-${JAVA_VERSION}-openjdk-devel" "java-${JAVA_VERSION}-openjdk-jmods" binutils;
 "/usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/jdeps" "--multi-release=${JAVA_VERSION}" -quiet --print-module-deps --ignore-missing-deps --recursive "${JAR_PATH}" > jre_modules.out;
-/usr/bin/xargs -a jre_modules.out "/usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/jlink" --compress=zip-9 --no-header-files --no-man-pages --strip-debug "--output=/tmp/${COMPONENT_PATH}/" --add-modules;
-cp -arv /tmp/${COMPONENT_PATH}/* "${COMPONENT_PAH}/";
+/usr/bin/xargs -a jre_modules.out "/usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/jlink" --compress=zip-9 --no-header-files --no-man-pages --strip-debug "--output=/tmp/microjre/" --add-modules;
+cp -arv /tmp/microjre/* "${COMPONENT_PATH}/";
