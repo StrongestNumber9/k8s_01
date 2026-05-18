6fcf47c2e6a9666dbd3c2e54c89556842f084fc8
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2026-05-18 14:03:40 +0300

    Try to do optimize stuff

---
+++ optimize/optimize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+JAVA_VERSION="${1}";
+JAR_PATH="${2}";
+COMPONENT_NAME="${3}";
+echo "Optimizing ${COMPONENT_NAME}:${JAR_PATH} on java version ${JAVA_VERSION}";
+dnf install -y java-${JAVA_VERSION}-openjdk-devel java-${JAVA_VERSION}-openjdk-jmods binutils;
+/usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/jdeps --multi-release=${JAVA_VERSION} -quiet --print-module-deps --ignore-missing-deps --recursive ${JAR_PATH} > jre_modules.out;
+/usr/bin/xargs -a jre_modules.out /usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/jlink --compress=zip-9 --no-header-files --no-man-pages --strip-debug --output=/opt/teragrep/${COMPONENT_NAME}/ --add-modules;
